ffad81f6d5966acc38ce78fe85c0917e5681cf48
Osama Abbas <pw.osama@gmail.com>
2022-12-14 21:09:40 +0900

    fix publicPath error

--- webpack/renderer.webpack.js
+++ webpack/renderer.webpack.js
@@ -2,6 +2,12 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
+  output: {
+    publicPath: '../',
+    environment: {
+      arrowFunction: false
+    }
+  },
   module: {
     rules: require('./rules.webpack'),
   },
